3ba363e155bd3d7fb375131aa232f6353094f33d
EmanElkholy6 <151935325+EmanElkholy6@users.noreply.github.com>
2023-12-21 14:42:34 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+  function valid(){
+	  var password ="document.getElementById("pass");
+	  var length =document.getElementById("length")
+	  if (password.value.length >=8{
+		  alert("login seccesfull");
+		  
+	  }
+	  else{
+		  alert("login Failed");
+	  }
+  }
